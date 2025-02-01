f73701e7c9e484c42330d68f7315c96cd4f7b5c9
Balazs Glatz <24852447+gbq6@users.noreply.github.com>
2025-02-01 14:34:16 +0100

    Refactored auth setup

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -5,59 +5,57 @@ import dotenv from 'dotenv';
 dotenv.config();
 const ten_seconds = 10_000;
 
-const users = {
-    user: {
-        email: process.env.USER_EMAIL,
-        password: process.env.USER_PASSWORD,
-    },
-    verified_user: {
-        email: process.env.VERIFIED_USER_EMAIL,
-        password: process.env.VERIFIED_USER_PASSWORD,
-    },
-}
-
-setup.describe('Authenticate user', async () => {
-    for (const [name, { email, password }] of Object.entries(users)) {
-        setup.skip(!email || !password, `no credentials for ${name}`);
-        setup(`authenticate as ${name}`, async ({ page }) => {
-            await page.goto(PUBLIC_URLS.login);
-            await page.locator('#loginform-email').fill(email);
-            await page.locator('#loginform-password').fill(password);
-            await page.locator('button[type="submit"]').click();
-
-            await page.waitForLoadState('networkidle', { timeout: ten_seconds });
-
-            const userMenuLink = await page.locator('.header__user-menu__link--button');
-            const href = await userMenuLink.getAttribute('href');
-            expect(href).toContain(PROTECTED_URLS.profile);
-
-            await page.context().storageState({ path: `playwright/.auth/${name}.json` });
-        });
-    }
-});
-
-const admins = {
+const AUTH = {
     admin: {
         email: process.env.ADMIN_EMAIL,
         password: process.env.ADMIN_PASSWORD,
+        loginUrl: 'citizen/login',
+        confirmLogin: confirmAdminLogin,
     },
     super_admin: {
         email: process.env.SUPER_ADMIN_EMAIL,
         password: process.env.SUPER_ADMIN_PASSWORD,
+        loginUrl: 'citizen/login',
+        confirmLogin: confirmAdminLogin,
+    },
+    user: {
+        email: process.env.USER_EMAIL,
+        password: process.env.USER_PASSWORD,
+        loginUrl: PUBLIC_URLS.login,
+        confirmLogin: confirmUserLogin,
+    },
+    verified_user: {
+        email: process.env.VERIFIED_USER_EMAIL,
+        password: process.env.VERIFIED_USER_PASSWORD,
+        loginUrl: PUBLIC_URLS.login,
+        confirmLogin: confirmUserLogin,
     },
 }
 
-setup.describe('Authenticate admin', async () => {
-    for (const [name, { email, password }] of Object.entries(admins)) {
+async function confirmAdminLogin(page) {
+    await page.waitForURL('citizen')
+
+    expect(page.url()).toBe(process.env.BASE_URL + 'citizen');
+}
+
+async function confirmUserLogin(page) {
+    await page.waitForLoadState('networkidle', { timeout: ten_seconds });
+
+    const userMenuLink = await page.locator('.header__user-menu__link--button');
+    const href = await userMenuLink.getAttribute('href');
+    expect(href).toContain(PROTECTED_URLS.profile);
+}
+
+setup.describe('Authenticate', async () => {
+    for (const [name, { email, password, loginUrl, confirmLogin }] of Object.entries(AUTH)) {
         setup.skip(!email || !password, `no credentials for ${name}`);
         setup(`authenticate as ${name}`, async ({ page }) => {
-            await page.goto('citizen/login');
+            await page.goto(loginUrl);
             await page.locator('#loginform-email').fill(email);
             await page.locator('#loginform-password').fill(password);
             await page.locator('button[type="submit"]').click();
 
-            await page.waitForURL('citizen')
-            expect(page.url()).toBe(process.env.BASE_URL + 'citizen');
+            await confirmLogin(page);
 
             await page.context().storageState({ path: `playwright/.auth/${name}.json` });
         });
