0eee19a975f4bb411b9bcb5b451e7b23b0ec7aac
Antal Lohár <142735334+antal-l@users.noreply.github.com>
2025-02-11 10:57:55 +0100

    Fix/login tests (#26)
    * login_tests fixed
    
    * login_tests fixed
    
    * Improved readability
    
    * Improved fail tests
    
    * Slightly improved success test
    
    ---------
    
    Co-authored-by: Balazs Glatz <24852447+gbq6@users.noreply.github.com>

--- tests/login_tests.spec.js
+++ tests/login_tests.spec.js
@@ -1,15 +1,21 @@
 import { setRandomViewport } from '../playwright.config';
 import { LoginPage } from '../pages/login_page';
 import { expect, test } from '@playwright/test';
+import { PROTECTED_URLS, PUBLIC_URLS } from './urls';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const BASE_URL = process.env.BASE_URL;
 const EMPTY_FIELD = '';
-const VALID_EMAIL = 'antaltesztelo@gmail.com';
-const VALID_PASSWORD = 'Teszt01';
+const VALID_EMAIL = process.env.USER_EMAIL
+const VALID_PASSWORD = process.env.USER_PASSWORD;
 const INVALID_EMAIL = 'invalid@example.com';
 const INVALID_PASSWORD = 'Password123';
 const ERROR_INVALID_CREDENTIALS = 'Hibás felhasználói név vagy jelszó';
 const ERROR_EMPTY_EMAIL = 'E-mail cím nem lehet üres.';
 const ERROR_EMPTY_PASSWORD = 'Jelszó nem lehet üres.';
+const TEN_SECONDS = 10_000;
 
 test.beforeEach(async ({ page }) => {
     const Login = new LoginPage(page);
@@ -25,44 +31,36 @@ test.beforeEach(async ({ page }) => {
     }
 });
 
-// TODO: Fix
-test.skip('login test with valid email and valid password', async ({ page }) => {
-    const Login = new LoginPage(page);
-    await Login.login(VALID_EMAIL, VALID_PASSWORD);
-    await page.waitForURL('');
-    await expect(Login.userImage || Login.userProfile).toBeVisible();
-});
-
 const loginErrorTests = [
     {
         description: 'valid email and invalid password',
         email: VALID_EMAIL,
         password: INVALID_PASSWORD,
-        errorMessage: ERROR_INVALID_CREDENTIALS,
+        errorMessage: [ERROR_INVALID_CREDENTIALS],
     },
     {
         description: 'invalid email and valid password',
         email: INVALID_EMAIL,
         password: VALID_PASSWORD,
-        errorMessage: ERROR_INVALID_CREDENTIALS,
+        errorMessage: [ERROR_INVALID_CREDENTIALS],
     },
     {
         description: 'invalid email and invalid password',
         email: INVALID_EMAIL,
         password: INVALID_PASSWORD,
-        errorMessage: ERROR_INVALID_CREDENTIALS,
+        errorMessage: [ERROR_INVALID_CREDENTIALS],
     },
     {
         description: 'empty email field and valid password',
         email: EMPTY_FIELD,
         password: VALID_PASSWORD,
-        errorMessage: ERROR_EMPTY_EMAIL,
+        errorMessage: [ERROR_EMPTY_EMAIL],
     },
     {
         description: 'valid email and empty password field',
         email: VALID_EMAIL,
         password: EMPTY_FIELD,
-        errorMessage: ERROR_EMPTY_PASSWORD,
+        errorMessage: [ERROR_EMPTY_PASSWORD],
     },
     {
         description: 'empty email field and empty password field',
@@ -72,19 +70,28 @@ const loginErrorTests = [
     },
 ];
 
-for (const { description, email, password, errorMessage } of loginErrorTests) {
-    test(`login test with ${description}`, async ({ page }) => {
+test.describe('Login Tests', () => {
+    test('should succeed with valid email and valid password', async ({ page }) => {
         const Login = new LoginPage(page);
-        await Login.login(email, password);
-        await page.waitForURL('');
-        await expect(Login.userImage || Login.userProfile).not.toBeVisible();
+        await Login.login(VALID_EMAIL, VALID_PASSWORD);
 
-        if (Array.isArray(errorMessage)) {
-            for (const msg of errorMessage) {
-                await expect(page.getByText(msg)).toBeVisible();
-            }
-        } else if (errorMessage) {
-            await expect(page.getByText(errorMessage)).toBeVisible();
-        }
+        await page.waitForLoadState('networkidle', { timeout: TEN_SECONDS });
+
+        const userMenuLink = await page.locator('.header__user-menu__link--button');
+        const href = await userMenuLink.getAttribute('href');
+        expect(href).toContain(PROTECTED_URLS.profile);
     });
-}
+
+    for (const { description, email, password, errorMessage } of loginErrorTests) {
+        test(`should fail with ${description}`, async ({ page }) => {
+            const Login = new LoginPage(page);
+            await Login.login(email, password);
+
+            await expect(page.url()).toBe(BASE_URL + PUBLIC_URLS.login);
+
+            for (const message of errorMessage) {
+                await expect(page.getByText(message)).toBeVisible();
+            }
+        });
+    }
+});
