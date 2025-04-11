70b9051679161dd57fef57a59c58e1a49cae3fc8
Antal Lohár <a123lohar@gmail.com>
2025-04-11 12:42:44 +0200

    Problem reporting page locators was updated

--- pages/problem_reporting_page.js
+++ pages/problem_reporting_page.js
@@ -8,14 +8,14 @@ export class ProblemReportingPage {
         this.citySelect = page.locator('id=city-dropdown');
         this.problemLocation = page.locator('id=reportform-user_location');
         this.mapBox = page.locator('section').filter({ hasText: 'Helyszín Település * Válassz' }).locator('img')
-        this.showOnMapButton = page.locator('xpath=//*[@id="report-create-form"]/div[3]/section[2]/div/div[1]/div[1]/div[3]/div[2]/div/button[2]');
-        this.fileUploadDropzone = page.locator('xpath=//*[@id="report-create-form"]/div[3]/section[3]/div[1]/div[1]/div/div[3]/div');
-        this.videoLinkInfo = page.locator('xpath=//*[@id="report-create-form"]/div[3]/section[3]/div[2]/div[1]/div[1]/div[1]/p/span[2]/a');
-        this.addVideoInput = page.locator('xpath=//*[@id="report-create-form"]/div[3]/section[3]/div[2]/div[1]/div[1]/div[2]/div/input');
+        this.showOnMapButton = page.getByRole('button', { name: 'Mutasd a térképen' });
+        this.fileUploadDropzone = page.locator('.file-upload');
+        this.videoLinkInfo = page.getByRole('link', { name: 'Oszd meg a linkjét velünk!' });
+        this.addVideoInput = page.locator('section').filter({ hasText: 'Képek * Dobd be ide a képeket' }).getByRole('textbox');
         this.lastName = page.locator('id=reportform-namelast');
         this.firstName = page.locator('id=reportform-namefirst');
         this.anonymousReportCheckbox = page.locator('.checkbox');
-        this.submitButton = page.locator('xpath=//*[@id="report-create-form"]/div[3]/section[4]/div/div[1]/div[3]/div/button');
+        this.submitButton = page.getByRole('button', { name: 'Elküldöm a bejelentést' });
     }
 
     async selectCategory(category) {
