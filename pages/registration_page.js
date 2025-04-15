e8846bdd5d888af7cd70d5bdcdd0b139473eeda1
Antal Lohár <142735334+antal-l@users.noreply.github.com>
2025-04-15 17:57:19 +0200

    registration_page was created (#33)

---
+++ pages/registration_page.js
@@ -0,0 +1,22 @@
+export class RegistrationPage {
+
+    constructor(page) {
+        this.page = page;
+        this.registrationLastName = page.locator('id=registrationform-last_name');
+        this.registrationFirstName = page.locator('id=registrationform-first_name');
+        this.registrationEmail = page.locator('id=registrationform-email');
+        this.registrationPassword = page.locator('id=registrationform-password');
+        this.registrationPasswordRepeat = page.locator('id=registrationform-password_repeat');
+        this.cookieIcon = page.locator('id=cookieIcon');
+        this.googleLoginButton = page.getByRole('link', { name: 'Google fiókoddal' });
+        this.togglePasswordIconFirst = page.locator('#registration-form').getByRole('img').nth(1);
+        this.togglePasswordIconSecond = page.locator('#registration-form').getByRole('img').nth(2);
+        this.privacyPolicyCheckbox = page.locator('label').filter({ hasText: 'Hozzájárulok, hogy a Járókel' }).locator('div').first();
+        this.termsOfUseCheckbox = page.locator('label').filter({ hasText: 'Elolvastam és megértettem a Járókelő felhasználási feltételeit.' }).locator('div').first();
+        this.subscribeToNewsletterCheckbox = page.locator('label').filter({ hasText: 'Feliratkozom a Járókelő hí' }).locator('div').first();
+        this.privacyPolicyLink = page.getByRole('link', { name: 'adatkezelési tájékoztatóját' });
+        this.termsOfUseLink = page.getByRole('link', { name: 'felhasználási feltételeit' });
+        this.registrationSubmitButton = page.getByRole('button', { name: 'Regisztráció' });
+        this.loginLink = page.getByRole('link', { name: 'Lépj be!' });
+    }
+}
