708fa890019f1375ac3e025274ca399b8e13758c
Antal Lohár <a123lohar@gmail.com>
2025-08-17 10:58:55 +0200

    Refactor login page visibility tests to use beforeAll and add extra http header

--- tests/login_page_elements_visibility_test.spec.js
+++ tests/login_page_elements_visibility_test.spec.js
@@ -1,22 +1,39 @@
 import { expect, test } from '@playwright/test';
 import { LoginPage } from '../pages/login_page';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-test.beforeEach(async ({ page }) => {
-    const Login = new LoginPage(page);
-    await Login.gotoLoginPage();
-    await Login.clearCookies();
-});
+let Login;
+const AUTH_TOKEN = process.env.JK_AUTH_TOKEN;
 
-test('Login page elements are visible', async ({ page }) => {
-    const Login = new LoginPage(page);
-    await expect(Login.loginIcon).toBeVisible();
-    await expect(Login.emailTextbox).toBeVisible();
-    await expect(Login.passwordTextbox).toBeVisible();
-    await expect(Login.loginSubmitButton).toBeVisible();
-    await expect(Login.showPasswordIcon).toBeVisible();
-    await expect(Login.googleLoginButton).toBeVisible();
-    await expect(Login.rememberMeCheckbox).toBeVisible();
-    await expect(Login.forgottenPasswordLink).toBeVisible();
-    await expect(Login.signUpLink).toBeVisible();
+test.describe('UI Elements Visibility', () => {
+    test.beforeAll(async ({ browser }) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        Login = new LoginPage(page);
+        await page.setExtraHTTPHeaders({
+            'Authorization': `Bearer ${AUTH_TOKEN}`
+        });
+        await Login.gotoLoginPage();
+        await Login.clearCookies();
+    });
+
+    const elementsToTest = [
+        { name: 'loginIcon' },
+        { name: 'emailTextbox' },
+        { name: 'passwordTextbox' },
+        { name: 'loginSubmitButton' },
+        { name: 'showPasswordIcon' },
+        { name: 'googleLoginButton' },
+        { name: 'rememberMeCheckbox' },
+        { name: 'forgottenPasswordLink' },
+        { name: 'signUpLink' },
+    ];
+
+    for (const element of elementsToTest) {
+        test(`${element.name} should be visible`, async () => {
+            await expect(Login[element.name]).toBeVisible();
+        });
+    }
 });
