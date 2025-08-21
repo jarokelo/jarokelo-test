431fd9f0dafa453b4c6ac0ac081e7018ef607479
Antal Lohár <142735334+antal-l@users.noreply.github.com>
2025-08-21 19:41:50 +0200

    Increase the number of CI workers from 1 to 4 for better test parallelism, and avoid using firefox or webkit for tests to run faster (#57)

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ module.exports = defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 4 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -45,7 +45,7 @@ module.exports = defineConfig({
             use: { ...devices['Desktop Chrome'] },
             dependencies: ['setup'],
         },
-
+        /*
         {
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
