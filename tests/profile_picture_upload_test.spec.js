882992dea609bac7e7fc1bcbe7f735b9cced831a
Antal Lohár <142735334+antal-l@users.noreply.github.com>
2025-09-09 14:37:12 +0200

    Refactor profile picture upload test by removing unnecessary login steps, imports and variables (#64)

--- tests/profile_picture_upload_test.spec.js
+++ tests/profile_picture_upload_test.spec.js
@@ -1,29 +1,17 @@
 import { expect, test } from '@playwright/test';
-import { MainPage } from '../pages/main_page';
-import { LoginPage } from '../pages/login_page';
 import { ProfileManagementPage } from '../pages/profile_management_page';
-import { ProfilePage } from '../pages/profile_page';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const VALID_EMAIL = process.env.USER_EMAIL;
-const VALID_PASSWORD = process.env.USER_PASSWORD;
-const FIVE_SECONDS = 5_000;
 const AUTH_TOKEN = process.env.JK_AUTH_TOKEN;
 
+test.use({ storageState: 'playwright/.auth/user.json' });
+
 test.beforeEach(async ({ page }) => {
-    const Login = new LoginPage(page);
     await page.setExtraHTTPHeaders({
         'Authorization': `Bearer ${AUTH_TOKEN}`
     });
-    await Login.gotoLoginPage();
-    await Login.clearCookies();
-    await Login.emailTextbox.fill(VALID_EMAIL);
-    await Login.passwordTextbox.fill(VALID_PASSWORD);
-    await Login.loginSubmitButton.click();
-    await page.waitForLoadState('networkidle', { timeout: FIVE_SECONDS });
-
 });
 
 const profileImages = [
@@ -33,11 +21,9 @@ const profileImages = [
 
 for (const image of profileImages) {
     test(`Profile picture upload and visual verification test with image: ${image}`, async ({ page }) => {
-        const main = new MainPage(page);
-        const profile = new ProfilePage(page);
         const profile_management = new ProfileManagementPage(page);
-        await main.userMenu.click();
-        await profile.editMyProfile.click();
+        await profile_management.gotoProfileManagementPage();
+        await profile_management.clearCookies();
         await page.setInputFiles('input[type="file"]', image);
         await page.reload();
         await expect(profile_management.profilePicture).toBeVisible();
